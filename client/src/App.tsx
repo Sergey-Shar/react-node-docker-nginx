8d193589a34ab46e04afa8f760d92b7b81762ec6
Sergey Sharuha <sergeihar82@gmail.com>
2023-04-05 14:58:32 +0300

    refactor: creacte not conrol inputs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,22 @@
-import React, { ChangeEventHandler, FormEventHandler, useState } from 'react'
 import axios from 'axios'
+import React, { ChangeEventHandler, FormEventHandler, useRef, useState } from 'react'
+
 import './App.css'
-
 const URL_AUTH = 'http://localhost:4000/api/auth'
-
 interface IAuthResponse {
   message: string
 }
 
 function App() {
-  const [values, setValue] = useState({ userName: '', password: '' })
   const [isDisable, setDisable] = useState(true)
+  const formValues = useRef({ userName: '', password: '' })
+  const formRef = useRef<HTMLFormElement | null >(null)
 
   const handleChange: ChangeEventHandler<HTMLFormElement> = ({
     target: { value, name }
   }) => {
-    setValue({ ...values, [name]: value })
-    const isDisable = Object.values(values).some((value) => !value.trim())
-    setDisable(isDisable)
+    formValues.current = { ...formValues.current, [name]: value }
+    setDisable(Object.values(formValues.current).some((value) => !value.trim()))
   }
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = async(
@@ -25,8 +24,7 @@ function App() {
   ): Promise<void> => {
     event.preventDefault()
     try {
-      const { data } = await axios.post<IAuthResponse>(URL_AUTH, values)
-      setValue({ userName: '', password: '' })
+      const { data } = await axios.post<IAuthResponse>(URL_AUTH, formValues.current)
       alert(data.message)
     } catch (error) {
       if (axios.isAxiosError(error)) {
@@ -34,16 +32,20 @@ function App() {
       }
       console.error(error)
     }
+    formRef.current?.reset()
   }
 
   return (
-    <form onSubmit={handleSubmit} onChange={handleChange}>
-      <input placeholder="name..." name="userName" type="text" value={values.userName} />
+    <form ref={formRef} onSubmit={handleSubmit} onChange={handleChange}>
+      <input
+        placeholder="name..."
+        name="userName"
+        type="text"
+      />
       <input
         placeholder="password"
         name="password"
         type="password"
-        value={values.password}
       />
       <button disabled={isDisable} type="submit">
         вход
